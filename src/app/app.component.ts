55fc0624eeaccdd4b5c789482019f3fbe86a2456
ajerbi <ahmedkhaliljerbi@gmail.com>
2018-02-11 22:40:37 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import { Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Item {
+  name: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  message = '';
+
+  constructor(private afs: AngularFirestore) {
+  }
+
+  ngOnInit() {
+    this.itemsCollection = this.afs.collection('messages');
+    this.items = this.itemsCollection.valueChanges();
+  }
+
+  chatSend(message) {
+    this.afs.collection('messages').add({'name': 'hi', 'message': message});
+    this.message = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireStorageModule} from 'angularfire2/storage';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBfK8GaoLCvNtczlE1X8xylqRmIn7zuums',
+  authDomain: 'chatapp-685ed.firebaseapp.com',
+  databaseURL: 'https://chatapp-685ed.firebaseio.com',
+  projectId: 'chatapp-685ed',
+  storageBucket: 'chatapp-685ed.appspot.com',
+  messagingSenderId: '873440199024'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
